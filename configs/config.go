4637076a98acf95ff0577f0212c7762d0373f72c
ext-s.prokopev <ext-s.prokopev@akrikhin.ru>
2023-05-09 13:45:00 +0300

    add env

--- configs/config.go
+++ configs/config.go
@@ -1,16 +1,25 @@
 package configs
 
-import "flag"
+import (
+	"flag"
+	"github.com/caarlos0/env/v8"
+	"log"
+)
 
 type AppConfig struct {
-	BaseURL string
-	Addr    string
+	BaseURL string `env:"BASE_URL"`
+	Addr    string `env:"SERVER_ADDRESS"`
 }
 
 func InitConfig() (AppConfig, error) {
 	var Config AppConfig
+
 	flag.StringVar(&Config.BaseURL, "b", "http://localhost:8080", "short URL base address")
 	flag.StringVar(&Config.Addr, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
+	err := env.Parse(&Config)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return Config, nil
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/caarlos0/env/v8 v8.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
