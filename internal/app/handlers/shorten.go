295ae6ef1363663435623f5a761bc98ab197f479
ext-s.prokopev <ext-s.prokopev@akrikhin.ru>
2023-05-28 19:40:47 +0300

    add JSON endpoint

--- internal/app/handlers/shorten.go
+++ internal/app/handlers/shorten.go
@@ -1,46 +1,62 @@
 package handlers
 
 import (
-	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/ProvoloneStein/go-url-shortener-service/internal/logger"
 	"github.com/asaskevich/govalidator"
 	"go.uber.org/zap"
+	"io"
 	"net/http"
 	"strings"
 )
 
-type getData struct {
-	url string `json: "url" valid: "url"`
+type requestData struct {
+	URL string `json:"url" valid:"url"`
+}
+
+type responseData struct {
+	Result string `json:"result" valid: "url"`
 }
 
 func (h *Handler) createShortURLByJSON(w http.ResponseWriter, r *http.Request) {
-	var requestData getData
-	var buf bytes.Buffer
+	var requestBody requestData
 
 	ct := r.Header.Get("Content-Type")
 	if !strings.HasPrefix(ct, "application/json") {
 		http.Error(w, "Неверный header", http.StatusBadRequest)
 		return
 	}
-	if err := json.Unmarshal(buf.Bytes(), &requestData); err != nil {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
 		http.Error(w, "Неверный запрос", http.StatusBadRequest)
 		return
 	}
-	if _, err := govalidator.ValidateStruct(requestData); err != nil {
+	if err := json.Unmarshal(body, &requestBody); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Неверный запрос", http.StatusBadRequest)
+		return
+	}
+	if _, err := govalidator.ValidateStruct(requestBody); err != nil {
 		http.Error(w, "Неверое тело запроса", http.StatusBadRequest)
 		return
 	}
-	res, err := h.services.CreateShortURL(requestData.url)
+	res, err := h.services.CreateShortURL(requestBody.URL)
 	if err != nil {
 		logger.Log.Error("ошибка при создании url", zap.Error(err))
 		http.Error(w, "Неверный запрос", http.StatusBadRequest)
 		return
 	}
+	b, err := json.Marshal(&responseData{Result: res})
+	if err != nil {
+		logger.Log.Error("ошибка при сериализации url", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	if _, err = w.Write([]byte(res)); err != nil {
+	if _, err = w.Write(b); err != nil {
 		logger.Log.Error("ошибка при создании url", zap.Error(err))
 	}
 }
