cb2c638265842f35c84be7a39fff7ef1474b7b9c
ext-s.prokopev <ext-s.prokopev@akrikhin.ru>
2023-10-03 11:58:28 +0300

    config

--- configs/config.go
+++ configs/config.go
@@ -5,21 +5,24 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/caarlos0/env/v8"
 	"io/fs"
+	"log"
+	"net"
 	"os"
 	"reflect"
-
-	"github.com/caarlos0/env/v8"
 )
 
 type AppConfig struct {
-	BaseURL        string `env:"BASE_URL" json:"base_url"`
-	Addr           string `env:"SERVER_ADDRESS" json:"server_address"`
-	FileStorage    string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
-	DatabaseDSN    string `env:"DATABASE_DSN" json:"database_dsn"`
-	SigningKey     string `env:"SIGNING_KEY" json:"signing_key"`
-	ConfigFileName string `env:"CONFIG" json:"-"`
-	EnableHTTPS    bool   `env:"ENABLE_HTTPS" json:"enable_https"`
+	BaseURL             string `env:"BASE_URL" json:"base_url"`
+	Addr                string `env:"SERVER_ADDRESS" json:"server_address"`
+	FileStorage         string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
+	DatabaseDSN         string `env:"DATABASE_DSN" json:"database_dsn"`
+	SigningKey          string `env:"SIGNING_KEY" json:"signing_key"`
+	ConfigFileName      string `env:"CONFIG" json:"-"`
+	TrustedSubnetString string `env:"TRUSTED_SUBNET" json:"trusted_subnet"`
+	EnableHTTPS         bool   `env:"ENABLE_HTTPS" json:"enable_https"`
+	TrustedSubnet       *net.IPNet
 }
 
 const signingKey = "qrkjk#4#%35FSFJlja#4353KSFjH"
@@ -47,7 +50,7 @@ func getFromFile(cfg *AppConfig) error {
 	return nil
 }
 
-func InitConfig() (AppConfig, error) {
+func New() (AppConfig, error) {
 	var config AppConfig
 
 	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "short URL base address")
@@ -57,6 +60,7 @@ func InitConfig() (AppConfig, error) {
 	flag.StringVar(&config.SigningKey, "k", signingKey, "service signing key")
 	flag.BoolVar(&config.EnableHTTPS, "s", false, "service signing key")
 	flag.StringVar(&config.ConfigFileName, "c", "configs/config.json", "config file name")
+	flag.StringVar(&config.TrustedSubnetString, "t", "", "trusted subnet")
 	flag.StringVar(&config.ConfigFileName, "config", "configs/config.json", "config file name")
 	flag.Parse()
 	err := env.Parse(&config)
@@ -69,6 +73,11 @@ func InitConfig() (AppConfig, error) {
 		if !errors.As(err, &pathError) {
 			return config, fmt.Errorf("ошибка при чтении файла конфигурации: %w", err)
 		}
+	}
+	_, config.TrustedSubnet, err = net.ParseCIDR(config.TrustedSubnetString)
+	if err != nil {
+		log.Printf("ошибка парсинга CIDR %w", err)
+		return config, nil
 	}
 
 	return config, nil
